ef4a1b40011766c234e4c89a10ce1e99ae86a6dc
sanghyeok1000 <parksang10000@naver.com>
2023-06-26 15:43:00 +0900

    Create 6:26.py

---
+++ 6:26.py
@@ -0,0 +1,44 @@
+'''
+날짜 : 6월 26일
+작성자 : 박상혁
+실습 내용 : 조건문, 터틀 그래픽
+'''
+import turtle as t
+t.shape("turtle")
+
+#삼각형 그리기
+t.color("blue")
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+
+#육각형 그리기
+t.color("red")
+t.fd(100)
+t.lt(60)
+t.fd(100)
+t.lt(60)
+t.fd(100)
+t.lt(60)
+t.fd(100)
+t.lt(60)
+t.fd(100)
+t.lt(60)
+t.fd(100)
+t.lt(60)
+
+#별 그리기
+t.color("yellow")
+t.fd(100)
+t.rt(144)
+t.fd(100)
+t.rt(144)
+t.fd(100)
+t.rt(144)
+t.fd(100)
+t.rt(144)
+t.fd(100)
+t.rt(144)
